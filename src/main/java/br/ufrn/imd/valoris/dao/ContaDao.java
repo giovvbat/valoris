19f679ae6b3d0d815288ceeacff7cd9d59a45d50
Giovanna Batista <giovanna.batista.124@ufrn.edu.br>
2025-05-13 20:17:18 -0300

    implementando o dao

--- src/main/java/br/ufrn/imd/valoris/dao/ContaDao.java
+++ src/main/java/br/ufrn/imd/valoris/dao/ContaDao.java
@@ -1,7 +1,24 @@
 package br.ufrn.imd.valoris.dao;
 
+import br.ufrn.imd.valoris.model.ContaModel;
 import org.springframework.stereotype.Repository;
+
+import java.util.*;
 
 @Repository
 public class ContaDao {
+    private final Map<String, ContaModel> contas = new HashMap<>();
+
+    public ContaModel saveConta(ContaModel contaModel) {
+        contas.put(contaModel.getNumero(), contaModel);
+        return contaModel;
+    }
+
+    public Optional<ContaModel> findByNumero(String numero) {
+        if (contas.containsKey(numero)) {
+            return Optional.of(contas.get(numero));
+        }
+
+        return Optional.empty();
+    }
 }
