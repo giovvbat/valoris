2127f0b73ff9b9186f5c47b60e97da65b7044a6e
Giovanna Batista <126830839+giovvbat@users.noreply.github.com>
2025-05-16 13:33:57 -0300

    mergeando pull request #9 (issue #1)

---
+++ src/main/java/br/ufrn/imd/valoris/dao/ContaDao.java
@@ -0,0 +1,26 @@
+package br.ufrn.imd.valoris.dao;
+
+import br.ufrn.imd.valoris.model.ContaModel;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Repository
+public class ContaDao {
+    private final Map<String, ContaModel> contas = new HashMap<>();
+
+    public ContaModel saveConta(ContaModel contaModel) {
+        contas.put(contaModel.getNumero(), contaModel);
+        return contaModel;
+    }
+
+    public Optional<ContaModel> findByNumero(String numero) {
+        if (contas.containsKey(numero)) {
+            return Optional.of(contas.get(numero));
+        }
+
+        return Optional.empty();
+    }
+}
