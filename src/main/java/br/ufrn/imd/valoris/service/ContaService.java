3edba1b942d1072fc55ca5f26bbb756530a53bf4
Pedro V. Pinho <pvpedrinho06@gmail.com>
2025-05-16 21:52:28 -0300

    implementação de cadastrar conta (#2)

--- src/main/java/br/ufrn/imd/valoris/service/ContaService.java
+++ src/main/java/br/ufrn/imd/valoris/service/ContaService.java
@@ -1,7 +1,22 @@
 package br.ufrn.imd.valoris.service;
 
+import br.ufrn.imd.valoris.dao.ContaDao;
+import br.ufrn.imd.valoris.model.ContaModel;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ContaService {
+    private final ContaDao contaDao;
+
+    public ContaService(ContaDao contaDao) {
+        this.contaDao = contaDao;
+    }
+
+    public ContaModel cadastrarConta(String numero) {
+        ContaModel conta = new ContaModel();
+        conta.setNumero(numero);
+        conta.setSaldo(0.0);
+
+        return contaDao.saveConta(conta);
+    }
 }
