230279683e5a5f6835d6ded67c08ddd0470dd616
Pedro V Pinho <89277343+pedropinho60@users.noreply.github.com>
2025-05-17 11:17:21 -0300

    mergeando pull request #10 (issue #2)

--- src/main/java/br/ufrn/imd/valoris/controller/ContaController.java
+++ src/main/java/br/ufrn/imd/valoris/controller/ContaController.java
@@ -1,9 +1,25 @@
 package br.ufrn.imd.valoris.controller;
 
+import br.ufrn.imd.valoris.dto.ContaDTO;
+import br.ufrn.imd.valoris.model.ContaModel;
+import br.ufrn.imd.valoris.service.ContaService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/contas")
 public class ContaController {
+    private final ContaService contaService;
+
+    public ContaController(ContaService contaService) {
+        this.contaService = contaService;
+    }
+
+    @PostMapping
+    public ContaModel cadastrarConta(@RequestBody @Valid ContaDTO contaDTO) {
+        return contaService.cadastrarConta(contaDTO);
+    }
 }
--- src/main/java/br/ufrn/imd/valoris/service/ContaService.java
+++ src/main/java/br/ufrn/imd/valoris/service/ContaService.java
@@ -1,7 +1,23 @@
 package br.ufrn.imd.valoris.service;
 
+import br.ufrn.imd.valoris.dao.ContaDao;
+import br.ufrn.imd.valoris.dto.ContaDTO;
+import br.ufrn.imd.valoris.model.ContaModel;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ContaService {
+    private final ContaDao contaDao;
+
+    public ContaService(ContaDao contaDao) {
+        this.contaDao = contaDao;
+    }
+
+    public ContaModel cadastrarConta(ContaDTO contaDTO) {
+        ContaModel conta = new ContaModel();
+        conta.setNumero(contaDTO.numero());
+        conta.setSaldo(0.0);
+
+        return contaDao.saveConta(conta);
+    }
 }
