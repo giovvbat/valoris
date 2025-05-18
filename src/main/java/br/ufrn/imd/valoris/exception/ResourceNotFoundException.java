2e7472258c3962e7af6c3ce763e17b13d7a33137
Giovanna Batista <giovanna.batista.124@ufrn.edu.br>
2025-05-18 13:39:02 -0300

    resolvendo bug de duplicacao de contas (#19)

--- src/main/java/br/ufrn/imd/valoris/exception/ResourceNotFoundException.java
+++ src/main/java/br/ufrn/imd/valoris/exception/ResourceNotFoundException.java
@@ -1,6 +1,6 @@
 package br.ufrn.imd.valoris.exception;
 
-public class ResourceNotFoundException extends RuntimeException{
+public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
         super(message);
     }
--- src/main/java/br/ufrn/imd/valoris/handler/GlobalExceptionHandler.java
+++ src/main/java/br/ufrn/imd/valoris/handler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package br.ufrn.imd.valoris.handler;
 
 import br.ufrn.imd.valoris.dto.RespostaApiDTO;
+import br.ufrn.imd.valoris.exception.ResourceAlreadyExistsException;
 import br.ufrn.imd.valoris.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,5 +13,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<RespostaApiDTO> handleResourceNotFoundException(ResourceNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new RespostaApiDTO(HttpStatus.NOT_FOUND, ex.getMessage()));
+    }
+
+    @ExceptionHandler(ResourceAlreadyExistsException.class)
+    public ResponseEntity<RespostaApiDTO> handleResourceAlreadyExistsException(ResourceAlreadyExistsException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(new RespostaApiDTO(HttpStatus.CONFLICT, ex.getMessage()));
     }
 }
