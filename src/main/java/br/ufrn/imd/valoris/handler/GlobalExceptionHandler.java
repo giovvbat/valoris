25ada55ad96e1d6370a085603b71d511a7a2d94a
Giovanna Batista <126830839+giovvbat@users.noreply.github.com>
2025-05-18 13:23:33 -0300

    resolvendo um erro de escrita no método (#17)
    Co-authored-by: Pedro V Pinho <89277343+pedropinho60@users.noreply.github.com>

--- src/main/java/br/ufrn/imd/valoris/handler/GlobalExceptionHandler.java
+++ src/main/java/br/ufrn/imd/valoris/handler/GlobalExceptionHandler.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<RespostaApiDTO> handleResourceNotFoundExceptiono(ResourceNotFoundException ex) {
+    public ResponseEntity<RespostaApiDTO> handleResourceNotFoundException(ResourceNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new RespostaApiDTO(HttpStatus.NOT_FOUND, ex.getMessage()));
     }
 }
