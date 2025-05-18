efa5ba3b87f54f140d5a22847aca9eebd5480f9a
wislaargolo <wislaargolo1@gmail.com>
2025-05-17 23:34:38 -0300

    implementação da transferencia entre contas (#6)

---
+++ src/main/java/br/ufrn/imd/valoris/dto/TransferenciaDTO.java
@@ -0,0 +1,8 @@
+package br.ufrn.imd.valoris.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public record TransferenciaDTO(@NotBlank String numeroDestino, @NotNull @Positive Double valor) {
+}
