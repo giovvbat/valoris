825d435df8d69f152e3e75657a226d3d5df23943
Giovanna Batista <giovanna.batista.124@ufrn.edu.br>
2025-05-22 10:16:46 -0300

    implementação da conta bônus (#23)

--- src/main/java/br/ufrn/imd/valoris/dto/ContaDTO.java
+++ src/main/java/br/ufrn/imd/valoris/dto/ContaDTO.java
@@ -1,6 +1,8 @@
 package br.ufrn.imd.valoris.dto;
 
+import br.ufrn.imd.valoris.enums.TipoConta;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record ContaDTO(@NotBlank String numero) {
+public record ContaDTO(@NotBlank String numero, @NotNull TipoConta tipoConta) {
 }
