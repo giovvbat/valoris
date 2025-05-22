3f78f45a68fad62479f9122c345af0093de21b4c
wislaargolo <42840292+wislaargolo@users.noreply.github.com>
2025-05-22 20:17:39 -0300

    mergeando pull request #33 (issue #25)

--- src/main/java/br/ufrn/imd/valoris/dto/TransacaoDTO.java
+++ src/main/java/br/ufrn/imd/valoris/dto/TransacaoDTO.java
@@ -1,7 +1,7 @@
 package br.ufrn.imd.valoris.dto;
 
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 
-public record TransacaoDTO(@NotNull @Positive Double valor) {
+public record TransacaoDTO(@PositiveOrZero @NotNull Double valor) {
 }
--- src/main/java/br/ufrn/imd/valoris/handler/GlobalExceptionHandler.java
+++ src/main/java/br/ufrn/imd/valoris/handler/GlobalExceptionHandler.java
@@ -6,8 +6,13 @@ import br.ufrn.imd.valoris.exception.ResourceAlreadyExistsException;
 import br.ufrn.imd.valoris.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -24,5 +29,17 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NotEnoughAccountBalanceException.class)
     public ResponseEntity<RespostaApiDTO> handleNotEnoughAccountBalanceException(NotEnoughAccountBalanceException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new RespostaApiDTO(HttpStatus.BAD_REQUEST, ex.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new LinkedHashMap<>();
+        errors.put("status", HttpStatus.BAD_REQUEST.toString());
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
