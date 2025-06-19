fb27d3ea5ec1ce7cf7d42579f613610c17d6e6cb
Pedro V Pinho <89277343+pedropinho60@users.noreply.github.com>
2025-06-18 22:01:25 -0300

    lançando a v3 para produção (issue #84; pr #86)

--- src/main/java/br/ufrn/imd/valoris/model/ContaModel.java
+++ src/main/java/br/ufrn/imd/valoris/model/ContaModel.java
@@ -1,22 +1,29 @@
 package br.ufrn.imd.valoris.model;
 
+import br.ufrn.imd.valoris.enums.TipoConta;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ContaModel {
     @NotBlank
-    protected String numero;
+    protected String number;
     @NotNull
-    protected Double saldo;
+    protected Double balance;
+    @NotNull
+    protected TipoConta type;
 
     public void debitar(@PositiveOrZero @NotNull Double valor) {
-        this.saldo = this.saldo - valor;
+        this.balance = this.balance - valor;
     }
 
     public void creditar(@PositiveOrZero @NotNull Double valor) {
-        this.saldo = this.saldo + valor;
+        this.balance = this.balance + valor;
     }
 }
